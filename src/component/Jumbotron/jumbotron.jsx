0a16926a3afd7322241e57bee5778733046373d8
Marina Master <marina777master@mail.ru>
2023-11-01 17:09:24 -0400

    basic of jumbotron

---
+++ src/component/Jumbotron/jumbotron.jsx
@@ -0,0 +1,14 @@
+import Home from "../home";
+
+const jumbotron = () => {
+    return (
+<div className="jumbotron">
+    <h1 className="display-4">A Warm Welcome!</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut Labore et dolore magna aliqua. </p>
+    <hr className="my-4">
+        <a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+</div>
+);
+};
+
+export default jumbotron;
