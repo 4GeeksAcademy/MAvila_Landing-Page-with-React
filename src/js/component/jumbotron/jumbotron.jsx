9a64f18b66d23df80ace550ada0fe20645eb54c7
Marina Master <marina777master@mail.ru>
2023-11-01 20:48:51 -0400

    change style of jumbotron

--- src/js/component/jumbotron/jumbotron.jsx
+++ src/js/component/jumbotron/jumbotron.jsx
@@ -3,13 +3,16 @@ import React from 'react';
 
 const jumbotron = () => {
     return (
-<div className="jumbotron">
-    <h1 className="display-4">A Warm Welcome!</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut Labore et dolore magna aliqua.</p>
-    <hr className="my-4"></hr>
-        <a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
-</div>
-)
+        <div className="row position-relative">
+            <div className="col-lg-7 position-absolute top-50 start-50 translate-middle">
+                <div className="jumbotron bg-light mt-4 pb-5">
+                    <h1 className="display-4 text-lg-left"><b>A Warm Welcome!</b></h1>
+                    <p >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut Labore et dolore magna aliqua.</p>
+                    <a className="btn btn-primary btn-lg" href="#" role="button">Call to action!</a>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default jumbotron;
