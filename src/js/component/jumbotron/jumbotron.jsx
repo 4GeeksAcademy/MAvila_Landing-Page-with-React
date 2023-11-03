8113a838748f83a0ef1a37474efa4226d62ccb39
Marina Master <marina777master@mail.ru>
2023-11-03 18:46:08 -0400

    push jumbotron

--- src/js/component/jumbotron/jumbotron.jsx
+++ src/js/component/jumbotron/jumbotron.jsx
@@ -6,9 +6,9 @@ const jumbotron = () => {
         <div className="row position-relative">
             <div className="col-lg-7 position-absolute top-50 start-50 translate-middle">
                 <div className="jumbotron bg-light mt-4 pb-5">
-                    <h1 className="display-4 text-lg-left"><b>A Warm Welcome!</b></h1>
+                    <h1 className="display-4 text-lg-left d-flex justify-content-start"><b>A Warm Welcome!</b></h1>
                     <p >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut Labore et dolore magna aliqua.</p>
-                    <a className="btn btn-primary btn-lg" href="#" role="button">Call to action!</a>
+                    <a className="btn btn-primary btn-lg  .align-items-start " href="#" role="button">Call to action!</a>
                 </div>
             </div>
         </div>
